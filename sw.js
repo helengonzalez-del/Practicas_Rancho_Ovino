41d649850b0fab4919ea5f2943e8a5d265241f3e
Helen Gonzalez <helen.gonzalez@uabc.edu.mx>
2026-04-04 23:58:01 -0700

    Fix PWA icons and service worker paths

--- sw.js
+++ sw.js
@@ -1,33 +1,20 @@
-const CACHE = 'rancho-ovino-v1';
+const CACHE = 'rancho-ovino-v2'; // 👈 cambia versión para forzar actualización
+
 const ASSETS = [
-  '/', '/index.html',
-  '/css/base.css', '/css/components.css', '/css/auth.css',
-  '/js/config.js', '/js/utils.js', '/js/animales.js',
-  '/js/reproduccion.js', '/js/produccion.js', '/js/salud.js',
-  '/js/ventas.js', '/js/dashboard.js', '/js/realtime.js',
-  '/js/app.js', '/js/auth.js'
+  './',
+  './index.html',
+  './css/base.css',
+  './css/components.css',
+  './css/auth.css',
+  './js/config.js',
+  './js/utils.js',
+  './js/animales.js',
+  './js/reproduccion.js',
+  './js/produccion.js',
+  './js/salud.js',
+  './js/ventas.js',
+  './js/dashboard.js',
+  './js/realtime.js',
+  './js/app.js',
+  './js/auth.js'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
