7af2f2ab484565d30bca063763eb796bc7ee22d1
Helen Gonzalez <helen.gonzalez@uabc.edu.mx>
2026-04-10 15:33:36 -0700

    deshabilitar cache service worker

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-const CACHE = 'rancho-ovino-v3';// 👈 cambia versión para forzar actualización
-
-const ASSETS = [
-  './',
-  './index.html',
-  './css/base.css',
-  './css/components.css',
-  './css/auth.css',
-  './js/config.js',
-  './js/utils.js',
-  './js/animales.js',
-  './js/reproduccion.js',
-  './js/produccion.js',
-  './js/salud.js',
-  './js/ventas.js',
-  './js/dashboard.js',
-  './js/realtime.js',
-  './js/app.js',
-  './js/auth.js'
-];
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
